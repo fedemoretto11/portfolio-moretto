773930b5a826e97890b2a6f0ca9df600fb224d6b
fedemoretto11 <fedemoretto94@gmail.com>
2023-12-20 12:07:15 -0300

    Algun cambio que tengo que revisar porque no se cual es exactamente

--- src/components/db/migrations.js
+++ src/components/db/migrations.js
@@ -2,11 +2,13 @@ import { addDoc, collection } from "firebase/firestore"
 import { db } from "./data"
 
 project = {
-  description: "Proyecto final del curso de JavaScript, que consiste en un carrito de compras completamente funcional. El proyecto ha sido desarrollado desde cero y cuenta con la integración de la API de Mercado Libre.",
-  githubLink: "https://github.com/fedemoretto11/SuperMusicChanguito",
-  webLink: "https://fedemoretto11.github.io/SuperMusicChanguito/",
-  title: "Super Music Changuito",
-  type: "varios",
+  description: "Sitio web para Corporación Capital, empresa de RRHH especializada en contratación de personal y soluciones para empleadores. Ofrece sección de postulación a empleos y contacto directo con empleadores.",
+  githubLink: "https://github.com/fedemoretto11/corporacion-capital",
+  webLink: "https://corporacion-capital.vercel.app/",
+  title: "Corporacion Capital - No terminado",
+  type: "sitioWeb",
+  img:"https://firebasestorage.googleapis.com/v0/b/portfolio-moretto.appspot.com/o/images%2FCorporacionCapital.png?alt=media&token=99393175-780d-4873-aeb8-fe9ce7fffb77",
+  isFinished: false,
 }
 
 function addToDB(){
